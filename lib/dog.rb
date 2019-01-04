0c6bcaeda01e57e71de3ef9cc694f4a2a28cd5b0
Sara Varisara <varisara.n@gmail.com>
2019-01-04 04:35:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Dog
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def bark
+    puts "Woof!"
+  end
+end
+
+fido = Dog.new
+fido.name = "Fido"
+fido.name
+fido.bark
